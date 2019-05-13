088d1f05915e39b287463e65d4cfc23c34f4e1e8
Keiana Dunn <43360865+ktd2001@users.noreply.github.com>
2019-05-12 22:14:34 -0400

    Add files via upload

--- map2.py
+++ map2.py
@@ -1,234 +1,236 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Mar 20 20:50:06 2019
+#Column numbers with zero indexing ( 0-9 )
+#1234567890123456789012345678901234567890123456789012345678901234567890123456789
+#Column numbers with one indexing (1-10; t = 10)
+#23456789t123456789t123456789t123456789t123456789t123456789t123456789t123456789t
 
-@author: ktd2001
-"""
-# -*- coding: utf-8 -*-
-# Import Modules
+# Import Modules 
 import dash
-import dash_table_experiments as dt
 import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
-#import plotly.graph_objs from *
-from dash.dependencies import Input, Output, State, Event
+
+from plotly import graph_objs as go
+from plotly.graph_objs import *
+from dash.dependencies import Input, Output
+
 
 # Create tab name in browser
 app = dash.Dash(__name__)
 server = app.server
-app.config['suppress_callback_exceptions']=True
-app.title = 'SF Resturante Inspection Analysis'
+app.title = 'SF Restaurant Inspection Analysis'
 
 # API key and datasets
-mapbox_access_token = 'pk.eyJ1Ijoia3RkMjAwMSIsImEiOiJjanUwZzBkZWYxYWdyNDRtcTB3M3Rqb2w2In0.HC_MMaor7bWiJEu7Ytp7pA'
-map_data = pd.read_csv("20restuarnte.dat")
-#df.head(10)
+mapbox_access_token = "pk.eyJ1Ijoia3RkMjAwMSIsImEiOiJjanUwZzBkZWYxYWdyNDRtcTB3M3Rqb2w2In0.HC_MMaor7bWiJEu7Ytp7pA"
 
-# Selecting desired columns
-map_data = map_data[["business_name", "business_address", "business_latitude", "business_longitude", "inspection_date", "risk_category", "inspection_score", "business_state", "business_postal_code"]].drop_duplicates()
-#map_data.head(5)
+
+# Read in all data
+df = pd.read_csv('restaurant_scores.csv')
+# Drop the rows containing NA
+df = df.dropna()
+# Drop rows in such a way as to retain only n randomly chosen rows (in this case n is set to 1000)
+df = df.sample(n=1000)
+
+df['text'] = df['business_name'] +"<br>"+ df['risk_category'] +"<br>"+ df['inspection_type']+"<br>"+ df['inspection_score'].astype(str)
+
+# Color Scale and color variables
+scl1 = [[0, 'red'],[0.5,"rgb(255, 255, 0)"], [1.0, 'green']]
+
+scl2 = [[0, 'green'],[0.5,"rgb(255, 255, 0)"],[1.0,"rgb(255, 0, 0)"] ]
+
+color_names= [' ','Low','Moderate','High']
+
+color_vals = list(range(len(color_names)))
+
+num_colors = len(color_vals)
+
+# Created new columns turn strings into numbers 
+def trans_risk_category(x):
+    if x == 'Low Risk':
+        return 1.0
+    if x == 'Moderate Risk':
+        return 2.0
+    if x == 'High Risk':
+        return 3.0
+    
+df['numerical_risk_category'] = df['risk_category'].apply(trans_risk_category) 
+
+
+# Setting up figure 1
+data1 = [
+        go.Scattermapbox(
+                        lon = df['business_longitude'],
+                        lat = df['business_latitude'],
+                        text = df['text'],
+                        mode='markers',
+                        marker=go.scattermapbox.Marker(
+                                                      size=14,
+                                                      colorscale = scl1,
+                                                      opacity=0.50,
+                                                      cmin = -0,
+                                                      color = df['inspection_score'],
+                                                      cmax = -100,
+                                                      colorbar=dict(title="Inspection Score"),
+                                                      )
+                        )
+        ]
+
+layout1 = go.Layout(title='Map of Inspection Scores for SF Restaurants', 
+		    autosize=False,
+    		    width=1800,
+                    height=500,
+    	 	    hovermode='closest',
+		    mapbox=dict(accesstoken=mapbox_access_token, 
+		    center=dict(lat=37.759174, lon=-122.418116), 
+		    style='dark',
+		    zoom=11.0)
+                   )
+
+fig1 = go.Figure(data=data1, layout=layout1 )
+
+
+# Setting up figure 2
+
+data2 = [
+        go.Scattermapbox(
+                        lon = df['business_longitude'],
+                        lat = df['business_latitude'],
+                        text = df['text'],
+                        mode='markers',
+                        marker=go.scattermapbox.Marker(
+						      size=14,
+                                                      colorscale = scl2,
+                                                      opacity=0.50,
+                                                      cmin = 1,
+                                                      color = df['numerical_risk_category'],
+                                                      cmax = 3,
+                                                      colorbar=dict(title="Risk Category",tickvals= color_vals, ticktext= color_names),
+                                                      )
+                        )
+        ]
+
+layout2 = go.Layout(title='Map of Risk Category of SF Restaurants', 
+		    autosize=True,
+    		    width=1800,
+                    height=600,
+    	 	    hovermode='closest',
+		    mapbox=dict(accesstoken=mapbox_access_token, 
+		    center=dict(lat=37.759174, lon=-122.418116), 
+		    style='dark',
+		    zoom=11.0)
+                   )
+
+fig2 = go.Figure(data=data2, layout=layout2 )
+
+# Setting up figure 3
+
+data3 = [go.Histogram(x=df['inspection_score'])]
+
+layout3 = go.Layout(title='Histogram for Restaurant Inspection Score Distribution', 
+                    autosize=False,
+                    width=1800,
+                    height=600,
+                    bargap=0.01,
+                    xaxis=go.layout.XAxis(title='Inspection Scores'),
+                    yaxis=go.layout.YAxis(title='Count')
+                    )
+
+fig3 = go.Figure(data=data3, layout=layout3)
+
+
+# Setting up figure 4
+
+data4 = [go.Histogram(x=df['numerical_risk_category'])]
+
+layout4 = go.Layout(title='Histogram for Restaurant Risk Category Distribution',
+		    autosize=False,
+                    width=1300,
+                    height=600,
+ 		    bargap=0.002, 
+                    xaxis=go.layout.XAxis(title='Risk Category: Low Risk = 1, Moderate Risk = 2, High Risk = 3'),
+                    yaxis=go.layout.YAxis(title='Count')
+                    )
+
+fig4 = go.Figure(data=data4, layout=layout4)
 
 # Boostrap CSS
 app.css.append_css({'external_url': 'https://codepen.io/amyoshino/pen/jzXypZ.css'})
+        
+#Header layout
+#Column numbers with zero indexing ( 0-9 )
+#1234567890123456789012345678901234567890123456789012345678901234567890123456789
+app.layout = html.Div([
+                       html.Div([
+                                html.H1(children='San Francisco Restaurant Visualization',
+                                className='main plot'),
+ 	
+                                #html.P(children="Created By: Keiana Dunn dunnk11@go.stockton.edu",
+                                #className='three columns',
+                                #style={
+                                #      'height': '6%',
+                                #      'width': '6%',
+                                #      'float': 'right',
+                                #      'position': 'relative',
+                                #      'padding-top': 2,
+                                #      'padding-right': 2
+                                #       }),
+                                html.Div(children='''Visualization of 1000 SF restaurant inspection results (randomly sampled from 53,000 records)'''
+                                         ,className='nine columns')
+                               	 ], style={'textAlign': "center"
+                                          }),#className="row"
+      	                      # ),
+      	                     
+        
 
-#Layouts
-    #Table layouts for font and colors
-layout_table = dict(
-    autosize=True,
-    height=500,
-    font=dict(color="#191A1A"),
-    titlefont=dict(color="#191A1A", size='14'),
-    margin=dict(
-        l=35,
-        r=35,
-        b=35,
-        t=45
-    ),
-    hovermode="closest",
-    plot_bgcolor='#fffcfc',
-    paper_bgcolor='#fffcfc',
-    legend=dict(font=dict(size=10), orientation='h'),
-)
+#Column numbers with zero indexing ( 0-9 )
+#1234567890123456789012345678901234567890123456789012345678901234567890123456789
+                       html.Div([ 
 
-layout_table['font-size'] = '12'
-layout_table['margin-top'] = '20'
+                                 dcc.Graph(id='graph1', figure=fig1)
 
-   # Map layouts
-layout_map = dict(
-    autosize=True,
-    height=500,
-    font=dict(color="#191A1A"),
-    titlefont=dict(color="#191A1A", size='14'),
-    margin=dict(
-        l=35,
-        r=35,
-        b=35,
-        t=45
-    ),
-    hovermode="closest",
-    plot_bgcolor='#fffcfc',
-    paper_bgcolor='#fffcfc',
-    legend=dict(font=dict(size=10), orientation='h'),
-    title='SF Resturante Inspection',
-    mapbox=dict(
-        accesstoken=mapbox_access_token,
-        style="light",
-        center=dict(
-            lon=-122.419066,
-            lat=37.759174
-        ),
-        zoom=10,
-    )
-)
+				 ]),
 
-# function
-def gen_map(map_data):
-     #groupby returns a dictionary mapping the values of the first field
-     #'classification' onto a list of record dictionaries with that
-     #classification value.
-    return {
-        "data": [{
-                "type": "scattermapbox",
-                "lat": list(map_data['business_latitude']),
-                "lon": list(map_data['business_longitude']),
-                "hoverinfo": "text",
-                "hovertext": [["business_name: {} <br>risk_category: {} <br>inspection_score: {}".format(i,j,k)]
-                                for i,j,k in zip(map_data['business_name'], map_data['risk_category'],map_data['inspection_score'])],
-                "mode": "markers",
-                "name": list(map_data['business_name']),
-                "marker": {
-                    "size": 6,
-                    "opacity": 0.7
-                }
-        }],
-        "layout": layout_map
-    }
+#Column numbers with zero indexing ( 0-9 )
+#1234567890123456789012345678901234567890123456789012345678901234567890123456789
+                       html.Div([ 
 
-#Header
-app.layout = html.Div(
-    html.Div([
-        html.Div(
-            [
-                html.H1(children='Maps and Tables',
-                        className='nine columns'),
-                html.Img(
-                    src="https://stockton.edu/relations/brand-guide/images/official-stockton-logo-alternate1-display.png",
-                    className='three columns',
-                    style={
-                        'height': '11%',
-                        'width': '11%',
-                        'float': 'right',
-                        'position': 'relative',
-                        'padding-top': 12,
-                        'padding-right': 20
-                    },
-                ),
-                html.Div(children='''
-                        San Fransico Resturante Inspection Analysis
-                        ''',
-                        className='nine columns'
-               )
-            ], className="row"
-        ),
+                                 dcc.Graph(id='graph3', figure=fig3) 
+
+                                 ]),
+
+#Column numbers with zero indexing ( 0-9 )
+#1234567890123456789012345678901234567890123456789012345678901234567890123456789
+                       html.Div([ 
+
+                                 dcc.Graph(id='graph2', figure=fig2) 
+
+                                 ]),
+
+#Column numbers with zero indexing ( 0-9 )
+#1234567890123456789012345678901234567890123456789012345678901234567890123456789
+                       html.Div([ 
+
+                                 dcc.Graph(id='graph4', figure=fig4) 
+
+                                 ]), 
+           	       html.Div([
+
+                   		 html.P('        Created by:Keiana Dunn       ', style = {'display': 'inline'}),
+                    		 html.A('dunnk11@go.stockton.edu', href = 'mailto:dunnk11@go.stockton.edu')
+                                 
+				 ], className = "twelve columns",
+                                   style = {'fontSize': 18, 'padding-top': 20}
+                                 ), 
+    
+
+
+#Column numbers with zero indexing ( 0-9 )
+#1234567890123456789012345678901234567890123456789012345678901234567890123456789
+
+                      ], className='first row')
 
 
 
-# Selectors
-html.Div(
-    [
-            html.Div(
-                [
-                    html.P('Select Risk Category:'),
-                    dcc.Checklist(
-                            id = 'category',
-                            options=[
-                                {'label': 'Low Risk', 'value': 'LR'},
-                                {'label': 'Moderate Risk', 'value': 'MR'},
-                                {'label': 'High Risk', 'value': 'HR'}
-                            ],
-                            values=['LR', 'MR', 'HR'],
-                            labelStyle={'display': 'inline-block'}
-                    ),
-                ], className='six columns',
-                style={'margin-top': '10'}
-         ),
-            html.Div(
-                [
-                    html.P('Zip Code:'),
-                    dcc.Dropdown(
-                        id = 'zip_code',
-                        options= [{'label': int(item),
-					      'value': int(item)}
-                                             for item in set(map_data['business_postal_code'])],
-                        multi=True,
-                        value=list(set(map_data['business_postal_code']))
-                    )
-                ],
-                className='six columns',
-                style={'margin-top': '10'}
-            )
- 	           ],
-               className='row'
-           ),
-# Map + table + Histogram
-    html.Div(
-        [
-            html.Div(
-                [
-                    dcc.Graph(id='map-graph',
-                                animate=True,
-                                style={'margin-top': '20'})
-                ], className = "six columns"
-            ),
-            html.Div(
-                [
-                    dt.DataTable(
-                        rows=map_data.to_dict('records'),
-                        columns=map_data.columns,
-                        row_selectable=True,
-                        filterable=True,
-                        sortable=True,
-                        selected_row_indices=[],
-                        id='datatable'),
-                ],
-                style = layout_table,
-                className="six columns"
-            ),
-            html.Div([
-                    dcc.Graph(
-                        id='bar-graph'
-                    )
-                ], className= 'twelve columns'
-                ),
-            html.Div(
-                [
-                    html.P('Keiana Dunn', style = {'display': 'inline'}),
-                    html.A('dunnk11@go.stockton.edu', href = 'mailto:dunnk11@go.stockton.edu')
-                ], className = "twelve columns",
-                    style = {'fontSize': 18, 'padding-top': 20}
-            )
-        ], className="row"
-    )
-], className= 'ten columns offset-by-one'))
-
-@app.callback(
-    dash.dependencies.Output('datatable', 'rows'),
-    [dash.dependencies.Input('zip_code', 'value')],
-     [dash.dependencies.Input('category', 'values')])
-def update_selected_row_indices(zip_code, category):
-    map_aux = map_data.copy()
-
-# Zip Code filter
-    map_aux = map_aux[map_aux['business_postal_code'].isin(zip_code)]
-
-# Risk Category filter
-    map_aux = map_aux[map_aux['risk_category'].isin(category)]
-
-    rows = map_aux.to_dict('records')
-    return rows
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
-
+# Server Startup #########################################################
+if __name__ == "__main__":
+    print('hello there map2.py')
+    app.run_server(debug=True,hostname='0.0.0.0',port=8050)
